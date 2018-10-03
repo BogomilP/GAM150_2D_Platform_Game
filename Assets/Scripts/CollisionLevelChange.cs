1548b5f853583dbb91093bef8329c4ffe8df1d21
BogomilP <BP218208@falmouth.ac.uk>
2018-10-03 12:54:37 +0100

    Added levels as well as a hazard system and an end goal.

---
+++ Assets/Scripts/CollisionLevelChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CollisionLevelChange : MonoBehaviour {
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // The scene manager loads the next level of the game when the Capsule touches the green circle.
+    }
+    
+}
---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Hazard : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        // The scene manager loads the same level after the player hits a hazerdous object and the level restarts. Im sure there are better and easier ways to do this but at my current level this seems logical.
+    }
+
+}
