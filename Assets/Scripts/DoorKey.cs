1600666a18d730f5bfe0a87c8c84da55be6ab1d2
BogomilP <bogomiljpetrov@gmail.com>
2018-10-22 16:17:47 +0100

    Fix

---
+++ Assets/Scripts/DoorKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DoorKey : MonoBehaviour
+{
+    public GameObject Door;
+    public GameObject Capsule;
+    void OnTriggerEnter(Collider Capsule)
+    {
+        Destroy(Door.gameObject);
+        Destroy(gameObject);
+    }
+
+}
