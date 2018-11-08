e776dbf5b15ede9361c6c74e5552fbba4310d503
BogomilP <BP218208@falmouth.ac.uk>
2018-11-08 20:35:08 +0000

    Game is almost finished. Added a lot of things and fixed the collectibles and end goal. Just need to make a few more levels.

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Collectibles : MonoBehaviour {
+    [SerializeField]
+    Text CollectibleText;
+    [SerializeField]
+    Text GotEverything;
+
+    public GameObject KeyDoor;
+    int CollectedObjects;
+    int TotalObjects;
+    
+
+    // Use this for initialization
+    private void Start() {
+        
+        CollectibleText.enabled = false;
+        GotEverything.enabled = false;
+        TotalObjects = GameObject.FindGameObjectsWithTag("Collectible").Length;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Collectible")
+        {
+            CollectedObjects += 1;
+            if (CollectedObjects <= TotalObjects)
+            {
+                CollectibleText.text = CollectedObjects.ToString() + " collected out of " + TotalObjects;
+                StopAllCoroutines();
+                StartCoroutine(showText());
+
+
+            }
+
+            if (CollectedObjects == TotalObjects)
+            {
+                GotEverything.text = "You can now find the key...";
+                StopAllCoroutines();
+                StartCoroutine(showText());
+                Destroy(KeyDoor.gameObject);
+
+            }
+        }
+    }
+    IEnumerator showText()
+    {
+        CollectibleText.enabled = true;
+        yield return new WaitForSeconds(2);
+        CollectibleText.enabled = false;
+        yield return new WaitForSeconds(2);
+        GotEverything.enabled = true;
+        yield return new WaitForSeconds(2);
+        GotEverything.enabled = false;
+    }
+}
---
+++ Assets/Scripts/MoveWithPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlatform : MonoBehaviour {
+
+    Transform OriginalValue;
+    private GameObject Player;
+    private void OnTriggerStay(Collider other)
+    {
+        Player = GameObject.Find("Capsule");
+        OriginalValue = Player.transform;
+    Player.transform.parent = gameObject.transform;
+}
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player.transform.parent = null;
+    }
+}
